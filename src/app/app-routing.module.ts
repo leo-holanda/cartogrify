f90725a5dcea95f20c076b873566b6a31ea443ca
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-06-29 16:00:17 -0300

    Add selection page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { SelectionPageComponent } from "./selection-page/selection-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: SelectionPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
