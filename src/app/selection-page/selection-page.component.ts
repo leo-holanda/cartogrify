fe8a2fb02450a6220c158962018820ad5b6d74df
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-06-29 16:00:17 -0300

    Add selection page component

---
+++ src/app/selection-page/selection-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-selection-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './selection-page.component.html',
+  styleUrls: ['./selection-page.component.scss']
+})
+export class SelectionPageComponent {
+
+}
