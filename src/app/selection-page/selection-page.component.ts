146af2d647e49ddceae255eb0ecc0697c99e1970
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-06-30 15:46:17 -0300

    Add initial landing page

--- src/app/selection-page/selection-page.component.ts
+++ src/app/selection-page/selection-page.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-selection-page',
+  selector: "app-selection-page",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './selection-page.component.html',
-  styleUrls: ['./selection-page.component.scss']
+  templateUrl: "./selection-page.component.html",
+  styleUrls: ["./selection-page.component.scss"],
 })
 export class SelectionPageComponent {
+  onSpotifyButtonClick(): void {
+    console.log("spotify button clicked");
+  }
 
+  onLastfmButtonClick(): void {
+    console.log("lastfm button clicked");
+  }
 }
