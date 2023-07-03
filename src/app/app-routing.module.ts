7c4b56424b8d377f6b5061906e69e613c0d3ad40
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-07-03 13:50:45 -0300

    Add user top artists request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { SelectionPageComponent } from "./selection-page/selection-page.component";
 import { AuthorizationGateComponent } from "./authorization-gate/authorization-gate.component";
+import { WorldMapComponent } from "./world-map/world-map.component";
 
 const routes: Routes = [
   { path: "", component: SelectionPageComponent },
   { path: "authorization", component: AuthorizationGateComponent },
+  { path: "worldmap", component: WorldMapComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 import { SelectionPageComponent } from "./selection-page/selection-page.component";
 import { AuthorizationGateComponent } from "./authorization-gate/authorization-gate.component";
 import { HttpClientModule } from "@angular/common/http";
+import { WorldMapComponent } from './world-map/world-map.component';
 
 @NgModule({
-  declarations: [AppComponent, SelectionPageComponent, AuthorizationGateComponent],
+  declarations: [AppComponent, SelectionPageComponent, AuthorizationGateComponent, WorldMapComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
