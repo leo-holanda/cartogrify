36d2007e5eaf1c94c8285eb32355f6a76ca30c82
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-07-03 13:34:07 -0300

    Add access token request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { SelectionPageComponent } from "./selection-page/selection-page.component";
+import { AuthorizationGateComponent } from "./authorization-gate/authorization-gate.component";
 
-const routes: Routes = [{ path: "", component: SelectionPageComponent }];
+const routes: Routes = [
+  { path: "", component: SelectionPageComponent },
+  { path: "authorization", component: AuthorizationGateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
