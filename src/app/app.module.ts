a8868bab0ed01cc97ae96467ba0cb8fc75024917
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-07-03 09:34:08 -0300

    Add spotify authorization first step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SelectionPageComponent } from "./selection-page/selection-page.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SelectionPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
