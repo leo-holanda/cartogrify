c6246b875d7f3b44425050a7985f4e5e1bf528e0
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-07-06 15:17:04 -0300

    Add initial web scraping

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { SpotifyService } from "../services/spotify.service";
 import { ArtistService } from "../services/artist.service";
+import { WebScraperService } from "../services/web-scraper.service";
 
 @Component({
   selector: "app-world-map",
@@ -8,7 +9,11 @@ import { ArtistService } from "../services/artist.service";
   styleUrls: ["./world-map.component.scss"],
 })
 export class WorldMapComponent implements OnInit {
-  constructor(private spotifyService: SpotifyService, private artistService: ArtistService) {}
+  constructor(
+    private spotifyService: SpotifyService,
+    private artistService: ArtistService,
+    private webScraper: WebScraperService
+  ) {}
 
   ngOnInit(): void {
     this.spotifyService.getTopArtistsNames().subscribe((topArtistsNames) => {
@@ -16,7 +21,14 @@ export class WorldMapComponent implements OnInit {
         const artistsWithUndefinedCountry = topArtistsNames.filter((name) =>
           artistsData?.some((artist) => artist.name !== name.toLocaleLowerCase())
         );
-        console.log(artistsWithUndefinedCountry);
+
+        if (artistsWithUndefinedCountry.length > 0) {
+          this.webScraper
+            .getArtistsCountryOfOrigin(artistsWithUndefinedCountry)
+            .subscribe((data) => {
+              console.log(data);
+            });
+        }
       });
     });
   }
