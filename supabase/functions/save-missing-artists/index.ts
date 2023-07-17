7ba2fbf2ecbb51933cf3b6282f564a4dcae1386c
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-07-17 14:11:23 -0400

    Add shared cors file

--- supabase/functions/save-missing-artists/index.ts
+++ supabase/functions/save-missing-artists/index.ts
@@ -4,13 +4,11 @@
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
-};
+import { corsHeaders } from "../_shared/cors.ts";
 
 serve(async (req: Request) => {
+  if (req.method === "OPTIONS") return new Response("ok", { headers: corsHeaders });
+
   try {
     const supabaseClient = createClient(
       Deno.env.get("SUPABASE_URL") ?? "",
