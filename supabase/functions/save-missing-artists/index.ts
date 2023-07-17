bcad0f9e00a81affeff79310bfcd74fa80264b71
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-07-17 11:08:33 -0400

    Add edge function to save missing artists

---
+++ supabase/functions/save-missing-artists/index.ts
@@ -0,0 +1,42 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
+};
+
+serve(async (req: Request) => {
+  try {
+    const supabaseClient = createClient(
+      Deno.env.get("SUPABASE_URL") ?? "",
+      Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
+      {
+        global: {
+          headers: { Authorization: `Bearer ${Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")}` },
+        },
+        auth: {
+          persistSession: false,
+        },
+      }
+    );
+
+    const { missingArtists } = await req.json();
+    const { error } = await supabaseClient.from("artists").insert(missingArtists);
+    if (error) throw error;
+
+    return new Response(undefined, {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
