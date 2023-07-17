5ca432e1e80f0021eec93c0d66dda7b63517b0c9
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-07-17 14:11:23 -0400

    Add shared cors file

--- supabase/functions/fetch-artists-page/index.ts
+++ supabase/functions/fetch-artists-page/index.ts
@@ -3,11 +3,7 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
-};
+import { corsHeaders } from "../_shared/cors.ts";
 
 function fetchArtistPage(artistName: string): Promise<string> {
   return new Promise((resolve, reject) => {
