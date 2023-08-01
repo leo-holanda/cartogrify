525e945c9cbfc4d063794e5c31cae068e56b89f0
Leonardo <leo.uldholanda@gmail.com>
2023-08-01 16:42:02 -0300

    Add fetch lastfm top artists service function

---
+++ src/app/shared/supabase.model.ts
@@ -0,0 +1,35 @@
+export interface LastFmTopArtists {
+  topartists: TopArtists;
+}
+
+interface TopArtists {
+  artist: Artist[];
+  "@attr": Attr2;
+}
+
+interface Artist {
+  streamable: string;
+  image: Image[];
+  mbid: string;
+  url: string;
+  playcount: string;
+  "@attr": Attr;
+  name: string;
+}
+
+interface Image {
+  size: string;
+  "#text": string;
+}
+
+interface Attr {
+  rank: string;
+}
+
+interface Attr2 {
+  user: string;
+  totalPages: string;
+  page: string;
+  perPage: string;
+  total: string;
+}
