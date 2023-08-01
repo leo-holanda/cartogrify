0e6ced346a0be0fd47c4d6d9a1aed19e0b41ae43
Leonardo <leo.uldholanda@gmail.com>
2023-08-01 15:20:38 -0300

    Improve initial page

--- src/app/authorization/authorization.module.ts
+++ src/app/authorization/authorization.module.ts
@@ -4,10 +4,11 @@ import { AuthorizationRoutingModule } from "./authorization-routing.module";
 import { SpotifyAuthService } from "./spotify-auth.service";
 import { PostLoginComponent } from "./post-login/post-login.component";
 import { LoginComponent } from "./login/login.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [PostLoginComponent, LoginComponent],
-  imports: [CommonModule, AuthorizationRoutingModule],
+  imports: [CommonModule, AuthorizationRoutingModule, FormsModule],
   providers: [SpotifyAuthService],
 })
 export class AuthorizationModule {}
