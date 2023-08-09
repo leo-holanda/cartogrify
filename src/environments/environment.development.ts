5f9d0bdfc13f8b1ad8c04f753b56d46948c444c4
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-09 16:45:11 -0300

    Replace page finder hosting to aws lambda

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
   SUPABASE_URL: "http://localhost:54321",
-  PAGE_FINDER_URL: "http://localhost:54321/functions/v1/fetch-artists-page",
+  PAGE_FINDER_URL: "https://lkjhlvgt5myd3udka4s2sbktqq0lqxry.lambda-url.us-east-1.on.aws/",
 };
