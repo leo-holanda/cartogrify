6d5116e763a5fff3262d65147c48f72b2490bc5c
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-10 14:08:19 -0300

    Add country of origin determination fail message

--- src/app/artists/artists.module.ts
+++ src/app/artists/artists.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from "@angular/common";
 import { ArtistsComponent } from "./artists.component";
 import { ArtistsRoutingModule } from "./artists-routing.module";
 import { StreamingModule } from "../streaming/streaming.module";
+import { MessagesModule } from "primeng/messages";
+import { ToastModule } from "primeng/toast";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
   declarations: [ArtistsComponent],
-  imports: [CommonModule, ArtistsRoutingModule, StreamingModule],
+  imports: [
+    CommonModule,
+    ArtistsRoutingModule,
+    StreamingModule,
+    MessagesModule,
+    ToastModule,
+    ButtonModule,
+  ],
   providers: [],
 })
 export class ArtistsModule {}
