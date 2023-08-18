84ed55069bbab8a80410ebb84bd21f81d7ac0b7a
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-18 18:00:05 -0300

    Add map color theme change functionality

--- src/app/artists/artists.module.ts
+++ src/app/artists/artists.module.ts
@@ -8,6 +8,10 @@ import { ToastModule } from "primeng/toast";
 import { ButtonModule } from "primeng/button";
 import { DialogService, DynamicDialogModule } from "primeng/dynamicdialog";
 import { TabMenuModule } from "primeng/tabmenu";
+import { OverlayPanelModule } from "primeng/overlaypanel";
+import { ListboxModule } from "primeng/listbox";
+import { RadioButtonModule } from "primeng/radiobutton";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [ArtistsComponent],
@@ -20,6 +24,10 @@ import { TabMenuModule } from "primeng/tabmenu";
     ButtonModule,
     TabMenuModule,
     DynamicDialogModule,
+    OverlayPanelModule,
+    ListboxModule,
+    RadioButtonModule,
+    FormsModule,
   ],
   providers: [DialogService],
 })
