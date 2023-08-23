a9957668576b14bed528d5a4bca6f097202c3c88
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-23 15:24:18 -0300

    Fix enviroment variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,6 @@ export const environment = {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
   SUPABASE_URL: "http://localhost:54321",
   PAGE_FINDER_URL: "https://lkjhlvgt5myd3udka4s2sbktqq0lqxry.lambda-url.us-east-1.on.aws/",
+  SPOTIFY_CLIENT_ID: "c8201434fef4436fb83dfa7bb2a7128d",
+  SPOTIFY_REDIRECT_URI: "http://localhost:4200/authorization",
 };
