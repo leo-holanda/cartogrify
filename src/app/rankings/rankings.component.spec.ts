82e13b28303afb7a5824b588e01d8518b371b50f
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-25 10:41:32 -0300

    Extract rankings to its own component

---
+++ src/app/rankings/rankings.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RankingsComponent } from './rankings.component';
+
+describe('RankingsComponent', () => {
+  let component: RankingsComponent;
+  let fixture: ComponentFixture<RankingsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RankingsComponent]
+    });
+    fixture = TestBed.createComponent(RankingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/rankings/rankings.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProgressBarModule } from "primeng/progressbar";
+import { TabMenuModule } from "primeng/tabmenu";
+import { RankingsComponent } from "./rankings.component";
+import { MessagesModule } from "primeng/messages";
+import { ButtonModule } from "primeng/button";
+import { DialogService, DynamicDialogModule } from "primeng/dynamicdialog";
+import { SidebarModule } from "primeng/sidebar";
+
+@NgModule({
+  declarations: [RankingsComponent],
+  imports: [
+    CommonModule,
+    ProgressBarModule,
+    TabMenuModule,
+    MessagesModule,
+    ButtonModule,
+    DynamicDialogModule,
+    SidebarModule,
+  ],
+  providers: [DialogService],
+  exports: [RankingsComponent],
+})
+export class RankingsModule {}
