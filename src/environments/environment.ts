6a866435629780cc4093b1be88d5d596c260c154
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-28 17:40:06 -0300

    Fix supabase url production enviroment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqa25qaWdrbnZsZXlmcHF3b2lzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg0ODAzMjgsImV4cCI6MjAwNDA1NjMyOH0.cO-zjhPoFgpKtD1VXDkbhf_fAn1vic9GFpkL-gm-gyY",
-  SUPABASE_URL: "https:vjknjigknvleyfpqwois.supabase.co",
+  SUPABASE_URL: "https://vjknjigknvleyfpqwois.supabase.co",
   PAGE_FINDER_URL: "https://lkjhlvgt5myd3udka4s2sbktqq0lqxry.lambda-url.us-east-1.on.aws/",
   SPOTIFY_CLIENT_ID: "da044d8c393e4d7698a8444b03c206bb",
   SPOTIFY_REDIRECT_URI: "https://cartogrify.web.app/authorization",
