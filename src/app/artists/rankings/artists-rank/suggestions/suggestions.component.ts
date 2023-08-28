8d73f5344c50696ec91e6f421bfd2609e256c3be
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-28 14:52:47 -0300

    Improve suggestions dialog

--- src/app/artists/rankings/artists-rank/suggestions/suggestions.component.ts
+++ src/app/artists/rankings/artists-rank/suggestions/suggestions.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from "@angular/core";
-import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { DropdownChangeEvent } from "primeng/dropdown";
 import { ArtistWithSuggestion, Artist } from "src/app/artists/artist.model";
 import { Country } from "src/app/country/country.model";
 import { CountryService } from "src/app/country/country.service";
-import { debounceTime, fromEvent } from "rxjs";
+import { Subject, debounceTime, fromEvent } from "rxjs";
 
 @Component({
-  selector: "app-suggestions",
+  selector: "ctg-suggestions",
   templateUrl: "./suggestions.component.html",
   styleUrls: ["./suggestions.component.scss"],
 })
 export class SuggestionsComponent implements OnInit {
-  artists: ArtistWithSuggestion[] = [];
+  @Input() artists: Artist[] = [];
+  @Input() shouldMakeSuggestions$!: Subject<boolean>;
+  @Output() shouldCloseDialog = new EventEmitter<boolean>();
+
+  artistsWithSuggestion: ArtistWithSuggestion[] = [];
   countries: Country[] = [];
   selectedCountryToBulkSuggest: Country | undefined;
   selectedArtistsToBulkSuggest: ArtistWithSuggestion[] = [];
+
   isMobile = window.innerWidth <= 768;
 
-  constructor(
-    private dynamicDialogConfig: DynamicDialogConfig<Artist[]>,
-    private dynamicDialogRef: DynamicDialogRef,
-    private countryService: CountryService
-  ) {}
+  constructor(private countryService: CountryService) {}
 
   ngOnInit(): void {
     fromEvent(window, "resize")
@@ -31,38 +31,37 @@ export class SuggestionsComponent implements OnInit {
         this.isMobile = window.innerWidth <= 768;
       });
 
-    if (this.dynamicDialogConfig.data) {
-      this.artists = [...this.dynamicDialogConfig.data].map((artist) => {
-        (artist as ArtistWithSuggestion).suggestedCountry = undefined;
-        return artist as ArtistWithSuggestion;
-      });
+    this.shouldMakeSuggestions$.subscribe((shouldMakeSuggestions) => {
+      if (shouldMakeSuggestions) {
+        const suggestions = this.artistsWithSuggestion.filter((artist) => artist.suggestedCountry);
+        suggestions.forEach((suggestion) => {
+          const matchedArtist = this.artists.find((artist) => suggestion.name == artist.name);
+          if (matchedArtist) matchedArtist.country = suggestion.suggestedCountry;
+        });
 
-      this.countries = this.countryService.geoJSON.features.map((feature) =>
-        this.countryService.createCountryFromFeature(feature)
-      );
-    }
+        this.countryService.saveSuggestions(
+          suggestions.map((suggestion) => {
+            return {
+              name: suggestion.name,
+              country: suggestion.suggestedCountry,
+            };
+          })
+        );
+      }
+
+      this.shouldCloseDialog.emit(true);
+    });
+
+    this.artistsWithSuggestion = [...this.artists].map((artist) => {
+      return { ...artist, suggestedCountry: undefined } as ArtistWithSuggestion;
+    });
+
+    this.countries = this.countryService.geoJSON.features.map((feature) =>
+      this.countryService.createCountryFromFeature(feature)
+    );
   }
 
   onBulkSuggestDropdownChange(event: DropdownChangeEvent): void {
     this.selectedArtistsToBulkSuggest.forEach((artist) => (artist.suggestedCountry = event.value));
-  }
-
-  onCancelButtonClick(): void {
-    this.dynamicDialogRef.close(false);
-  }
-
-  onConfirmButtonClick(): void {
-    const suggestions = this.artists.filter((artist) => artist.suggestedCountry);
-    suggestions.forEach((artist) => (artist.country = artist.suggestedCountry));
-
-    this.countryService.saveSuggestions(
-      suggestions.map((suggestion) => {
-        return {
-          name: suggestion.name,
-          country: suggestion.suggestedCountry,
-        };
-      })
-    );
-    this.dynamicDialogRef.close(true);
   }
 }
--- src/app/artists/rankings/rankings.module.ts
+++ src/app/artists/rankings/rankings.module.ts
@@ -5,12 +5,13 @@ import { TabMenuModule } from "primeng/tabmenu";
 import { RankingsComponent } from "./rankings.component";
 import { MessagesModule } from "primeng/messages";
 import { ButtonModule } from "primeng/button";
-import { DialogService, DynamicDialogModule } from "primeng/dynamicdialog";
+import { DialogService } from "primeng/dynamicdialog";
 import { SidebarModule } from "primeng/sidebar";
 import { CountriesRankComponent } from "./countries-rank/countries-rank.component";
 import { RegionsRankComponent } from "./regions-rank/regions-rank.component";
 import { ArtistsRankComponent } from "./artists-rank/artists-rank.component";
 import { SuggestionsModule } from "./artists-rank/suggestions/suggestions.module";
+import { DialogModule } from "primeng/dialog";
 
 @NgModule({
   declarations: [
@@ -25,9 +26,9 @@ import { SuggestionsModule } from "./artists-rank/suggestions/suggestions.module
     TabMenuModule,
     MessagesModule,
     ButtonModule,
-    DynamicDialogModule,
     SidebarModule,
     SuggestionsModule,
+    DialogModule,
   ],
   providers: [DialogService],
   exports: [RankingsComponent],
