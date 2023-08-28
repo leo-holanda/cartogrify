4d8807dbbe608dfc102d68f7e2b72ab68f966b63
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-28 19:52:05 -0300

    Fix strange error in lastfm edge function

--- supabase/functions/fetch-lastfm-top-artists/index.ts
+++ supabase/functions/fetch-lastfm-top-artists/index.ts
@@ -8,10 +8,9 @@ import { corsHeaders } from "../_shared/cors.ts";
 serve(async (req) => {
   if (req.method === "OPTIONS") return new Response("ok", { headers: corsHeaders });
 
-  const { name } = await req.json();
-  const lastFmApiKey = Deno.env.get("LASTFM_API_KEY") ?? "";
-
   try {
+    const { name } = await req.json();
+    const lastFmApiKey = Deno.env.get("LASTFM_API_KEY") ?? "";
     const response = await fetch(
       `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${name}&api_key=${lastFmApiKey}&format=json`
     );
