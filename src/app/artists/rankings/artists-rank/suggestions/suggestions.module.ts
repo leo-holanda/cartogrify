8e846974e16945b827e53594b13e60c3a9b86d72
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-28 12:35:10 -0300

    Improve suggestion by bulk guide

--- src/app/artists/rankings/artists-rank/suggestions/suggestions.module.ts
+++ src/app/artists/rankings/artists-rank/suggestions/suggestions.module.ts
@@ -6,10 +6,19 @@ import { DropdownModule } from "primeng/dropdown";
 import { FormsModule } from "@angular/forms";
 import { TabViewModule } from "primeng/tabview";
 import { ButtonModule } from "primeng/button";
+import { ChipModule } from "primeng/chip";
 
 @NgModule({
   declarations: [SuggestionsComponent],
-  imports: [CommonModule, TableModule, FormsModule, DropdownModule, TabViewModule, ButtonModule],
+  imports: [
+    CommonModule,
+    TableModule,
+    FormsModule,
+    DropdownModule,
+    TabViewModule,
+    ButtonModule,
+    ChipModule,
+  ],
   exports: [SuggestionsComponent],
 })
 export class SuggestionsModule {}
