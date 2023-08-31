c9a7d48fec8607b954f7264a12a56c7310f19471
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-31 10:49:04 -0300

    Add secont act

---
+++ src/app/journey/second-act/second-act.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SecondActComponent } from "./second-act.component";
+
+@NgModule({
+  declarations: [SecondActComponent],
+  imports: [CommonModule],
+  exports: [SecondActComponent],
+})
+export class SecondActModule {}
