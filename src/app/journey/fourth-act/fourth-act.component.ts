c52de44809e415d44ef2b0448e07ec373f145ee8
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-08-31 14:31:36 -0300

    Add fourth act

---
+++ src/app/journey/fourth-act/fourth-act.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "ctg-fourth-act",
+  templateUrl: "./fourth-act.component.html",
+  styleUrls: ["./fourth-act.component.scss"],
+})
+export class FourthActComponent {}
