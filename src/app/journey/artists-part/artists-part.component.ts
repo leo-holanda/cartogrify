da594144b994a02f4ca9c46764f02ecbf5d4f691
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-02 21:29:01 -0300

    Improve artists part responsiveness

--- src/app/journey/artists-part/artists-part.component.ts
+++ src/app/journey/artists-part/artists-part.component.ts
@@ -1,17 +1,26 @@
-import { Component } from "@angular/core";
-import { Subject } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+import { Subject, debounceTime, fromEvent } from "rxjs";
 
 @Component({
   selector: "ctg-artists-part",
   templateUrl: "./artists-part.component.html",
   styleUrls: ["./artists-part.component.scss"],
 })
-export class ArtistsPartComponent {
+export class ArtistsPartComponent implements OnInit {
   shouldMakeSuggestions$ = new Subject<boolean>();
   shouldOpenDialog = false;
   hasHappyNoises = false;
+  isMobile = window.innerWidth <= 1280;
 
   constructor() {}
+
+  ngOnInit(): void {
+    fromEvent(window, "resize")
+      .pipe()
+      .subscribe(() => {
+        this.isMobile = window.innerWidth <= 1280;
+      });
+  }
 
   openSuggestionDialog(): void {
     this.shouldOpenDialog = true;
