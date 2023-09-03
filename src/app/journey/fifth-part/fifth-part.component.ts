68f3fbff7eca302b41a03b8928f91d1ea82a5dcd
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-02 21:32:55 -0300

    Change fifth part component name

--- src/app/journey/fifth-part/fifth-part.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "ctg-fifth-part",
-  templateUrl: "./fifth-part.component.html",
-  styleUrls: ["./fifth-part.component.scss"],
-})
-export class FifthPartComponent {}
---
+++ src/app/journey/map-part/map-part.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "ctg-map-part",
+  templateUrl: "./map-part.component.html",
+  styleUrls: ["./map-part.component.scss"],
+})
+export class MapPartComponent {}
