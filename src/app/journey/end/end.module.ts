4dc86a24fe40328d73c7e01134b5e6f6914f68d1
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-04 09:24:25 -0300

    Add journey end

---
+++ src/app/journey/end/end.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { EndComponent } from "./end.component";
+
+@NgModule({
+  declarations: [EndComponent],
+  imports: [CommonModule],
+  exports: [EndComponent],
+})
+export class EndModule {}
