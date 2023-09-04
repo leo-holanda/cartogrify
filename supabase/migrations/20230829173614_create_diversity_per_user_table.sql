f2164e2affd774f9cdaba41dca0c16fee3da9909
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-04 14:31:17 -0300

    Update migrations

--- supabase/migrations/20230829173614_create_diversity_per_user_table.sql
+++ supabase/migrations/20230829173614_create_diversity_per_user_table.sql
@@ -1,7 +1,8 @@
 create table
   public.diversity_per_user (
-    countries_count smallint not null,
+    user_countries_count smallint not null,
     user_id text not null,
-    user_country text null,
+    user_country_code integer null,
+    constraint diversity_per_user_pkey primary key (user_id),
     constraint diversity_per_country_quantity_user_id_key unique (user_id)
   ) tablespace pg_default;
