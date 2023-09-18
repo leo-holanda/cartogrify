92ee320c0409feb8278acafb4266bb26b255c573
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-18 18:13:30 -0300

    Add progress bar to show country popularity scale

--- src/app/journey/countries-popularity-rank/countries-popularity-rank.module.ts
+++ src/app/journey/countries-popularity-rank/countries-popularity-rank.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CountriesPopularityRankComponent } from "./countries-popularity-rank.component";
+import { ProgressBarModule } from "primeng/progressbar";
 
 @NgModule({
   declarations: [CountriesPopularityRankComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ProgressBarModule],
   exports: [CountriesPopularityRankComponent],
 })
 export class CountriesPopularityRankModule {}
