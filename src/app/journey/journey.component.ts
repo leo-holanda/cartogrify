3f053fd4fcbfe50fac5d01ae8702a7a309d43409
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-19 11:24:49 -0300

    Remove unused services in journey component

--- src/app/journey/journey.component.ts
+++ src/app/journey/journey.component.ts
@@ -1,12 +1,8 @@
 import { Component } from "@angular/core";
-import { ArtistService } from "../artists/artist.service";
-import { CountryService } from "../country/country.service";
 
 @Component({
   selector: "ctg-journey",
   templateUrl: "./journey.component.html",
   styleUrls: ["./journey.component.scss"],
 })
-export class JourneyComponent {
-  constructor(private artistService: ArtistService, private countryService: CountryService) {}
-}
+export class JourneyComponent {}
