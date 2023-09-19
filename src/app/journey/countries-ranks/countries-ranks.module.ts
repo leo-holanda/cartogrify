3d74682ad3ea8aafae5b8b69e9e954bf1a9ba2b0
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-19 11:10:14 -0300

    Change countries popularity rank folder location

--- src/app/journey/countries-ranks/countries-ranks.module.ts
+++ src/app/journey/countries-ranks/countries-ranks.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CountriesRanksComponent } from "./countries-ranks.component";
 import { CountriesDiversityRankModule } from "src/app/artists/rankings/countries-diversity-rank/countries-diversity-rank.module";
-import { CountriesPopularityRankModule } from "../countries-popularity-rank/countries-popularity-rank.module";
+import { CountriesPopularityRankModule } from "src/app/country/countries-popularity-rank/countries-popularity-rank.module";
 
 @NgModule({
   declarations: [CountriesRanksComponent],
