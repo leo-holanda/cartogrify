e992932841124e02008e13b027a75895d6b22bc4
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-20 13:51:44 -0300

    Fix countries count not updating

--- src/app/journey/journey.component.ts
+++ src/app/journey/journey.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { ArtistService } from "../artists/artist.service";
-import { CountryService } from "../country/country.service";
-import { RegionService } from "../region/region.service";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "ctg-journey",
   templateUrl: "./journey.component.html",
   styleUrls: ["./journey.component.scss"],
 })
-export class JourneyComponent implements OnInit {
-  constructor(
-    private artistService: ArtistService,
-    private countryService: CountryService,
-    private regionService: RegionService
-  ) {}
-
-  ngOnInit(): void {
-    this.artistService.getUserTopArtists().subscribe((userTopArtists) => {
-      this.countryService.updateCountriesCount(userTopArtists);
-      this.regionService.updateRegionsCount(userTopArtists);
-    });
-  }
-}
+export class JourneyComponent {}
