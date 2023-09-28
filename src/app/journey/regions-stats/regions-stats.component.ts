dad21dd563017770a149cb69537e50b3b20c1f9e
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-28 19:44:04 -0300

    Add regions diversity data in regions stats

--- src/app/journey/regions-stats/regions-stats.component.ts
+++ src/app/journey/regions-stats/regions-stats.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable, switchMap, take } from "rxjs";
+import { RegionService } from "src/app/region/region.service";
+import { ComparedDiversityData } from "src/app/statistics/statistics.model";
 import { StatisticsService } from "src/app/statistics/statistics.service";
 
 @Component({
@@ -7,11 +10,20 @@ import { StatisticsService } from "src/app/statistics/statistics.service";
   styleUrls: ["./regions-stats.component.scss"],
 })
 export class RegionsStatsComponent implements OnInit {
-  constructor(private statisticsService: StatisticsService) {}
+  comparedRegionDiversity!: Observable<ComparedDiversityData>;
+
+  constructor(private statisticsService: StatisticsService, private regionService: RegionService) {}
 
   ngOnInit(): void {
     this.statisticsService.getRegionsDiversity().subscribe((regionsDiversity) => {
       console.log(regionsDiversity);
     });
+
+    this.comparedRegionDiversity = this.regionService.getRegionsDiversity().pipe(
+      take(1),
+      switchMap((diversityData) => {
+        return this.statisticsService.getComparedRegionsDiversity(diversityData.regions);
+      })
+    );
   }
 }
