344a10adf4caf91724989435076f15bd05835ea4
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-29 14:17:32 -0300

    Remove stats selector when artists source is spotify

--- src/app/journey/geographic-stats/geographic-stats.component.ts
+++ src/app/journey/geographic-stats/geographic-stats.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { SelectItem } from "primeng/api";
 import { SelectButtonOptionClickEvent } from "primeng/selectbutton";
+import { ArtistsSources } from "src/app/artists/artist.model";
+import { ArtistService } from "src/app/artists/artist.service";
 
 @Component({
   selector: "ctg-geographic-stats",
   templateUrl: "./geographic-stats.component.html",
   styleUrls: ["./geographic-stats.component.scss"],
 })
-export class GeographicStatsComponent {
+export class GeographicStatsComponent implements OnInit {
+  userArtistsSource!: ArtistsSources;
+  artistsSource = ArtistsSources;
+
+  constructor(private artistService: ArtistService) {}
+
+  ngOnInit(): void {
+    this.userArtistsSource = this.artistService.getSource();
+  }
+
   chartOptions: SelectItem[] = [
     {
       label: "Continents",
