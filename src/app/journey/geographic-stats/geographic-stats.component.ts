57ae2a7a05e8971aafb12c52fd68a637ff5c6a5b
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-09-29 11:59:31 -0300

    Include regions and subregions stats in geographic stats

--- src/app/journey/geographic-stats/geographic-stats.component.ts
+++ src/app/journey/geographic-stats/geographic-stats.component.ts
@@ -1,8 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { SelectItem } from "primeng/api";
+import { SelectButtonOptionClickEvent } from "primeng/selectbutton";
 
 @Component({
   selector: "ctg-geographic-stats",
   templateUrl: "./geographic-stats.component.html",
   styleUrls: ["./geographic-stats.component.scss"],
 })
-export class GeographicStatsComponent {}
+export class GeographicStatsComponent {
+  chartOptions: SelectItem[] = [
+    {
+      label: "Continents",
+      value: "continents",
+    },
+    {
+      label: "Regions",
+      value: "regions",
+    },
+  ];
+
+  chartToShow = this.chartOptions[0].label;
+
+  onSelect(event: SelectButtonOptionClickEvent): void {
+    this.chartToShow = event.option.label;
+  }
+}
