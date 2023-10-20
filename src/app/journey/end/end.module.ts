60247089e771649686b4017aff852f61a6c5bcc8
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-10-20 11:24:48 -0300

    Add log out button

--- src/app/journey/end/end.module.ts
+++ src/app/journey/end/end.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { EndComponent } from "./end.component";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
   declarations: [EndComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   exports: [EndComponent],
 })
 export class EndModule {}
