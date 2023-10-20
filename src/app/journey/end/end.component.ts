a0bd7820235cd38f0e1a9e7087af84ca22dc395e
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-10-20 11:24:48 -0300

    Add log out button

--- src/app/journey/end/end.component.ts
+++ src/app/journey/end/end.component.ts
@@ -1,8 +1,16 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "ctg-end",
   templateUrl: "./end.component.html",
   styleUrls: ["./end.component.scss"],
 })
-export class EndComponent {}
+export class EndComponent {
+  constructor(private router: Router) {}
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(["/"]);
+  }
+}
