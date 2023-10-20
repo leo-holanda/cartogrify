ca74f52247fc08a4c12801f72a32c4a9880a6bdd
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-10-20 13:32:40 -0300

    Hide stats components when source is spotify

--- src/app/journey/journey.component.ts
+++ src/app/journey/journey.component.ts
@@ -1,8 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ArtistService } from "../artists/artist.service";
+import { ArtistsSources } from "../artists/artist.model";
 
 @Component({
   selector: "ctg-journey",
   templateUrl: "./journey.component.html",
   styleUrls: ["./journey.component.scss"],
 })
-export class JourneyComponent {}
+export class JourneyComponent implements OnInit {
+  artistSource!: ArtistsSources;
+  artistsSources = ArtistsSources;
+
+  constructor(private artistService: ArtistService) {}
+
+  ngOnInit(): void {
+    this.artistSource = this.artistService.getSource();
+  }
+}
