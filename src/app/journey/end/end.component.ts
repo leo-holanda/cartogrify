4d7f38ab8ea096df32f10df8b8304c75fbd26a2d
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-10-20 13:01:33 -0300

    Add account disconnect link

--- src/app/journey/end/end.component.ts
+++ src/app/journey/end/end.component.ts
@@ -1,13 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { ArtistsSources } from "src/app/artists/artist.model";
+import { ArtistService } from "src/app/artists/artist.service";
 
 @Component({
   selector: "ctg-end",
   templateUrl: "./end.component.html",
   styleUrls: ["./end.component.scss"],
 })
-export class EndComponent {
-  constructor(private router: Router) {}
+export class EndComponent implements OnInit {
+  artistSource!: ArtistsSources;
+  artistsSource = ArtistsSources;
+
+  constructor(private router: Router, private artistService: ArtistService) {}
+
+  ngOnInit(): void {
+    this.artistSource = this.artistService.getSource();
+  }
 
   logout(): void {
     localStorage.clear();
